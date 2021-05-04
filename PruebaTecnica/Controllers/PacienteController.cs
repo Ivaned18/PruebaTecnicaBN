69700fc376b944e2d42a29d3fd96e56085cef43d
ivaned18 <ivaned18@outlook.com>
2021-05-04 11:33:00 -0400

    Publicacion

--- PruebaTecnica/Controllers/PacienteController.cs
+++ PruebaTecnica/Controllers/PacienteController.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PruebaTecnica.Data;
